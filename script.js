af52c0ef4608165e8195f1a780b1a632dc0f5aca
Farhan Kamal <21619949-FarhanKamal2@users.noreply.replit.com>
2024-03-13 15:09:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function myfunction() {
+    document.getElementById("greet").style.color="purple";
+  document.body.style.backgroundColor="red";
+}
+function myfunction2() {
+  document.getElementById("TEST").style.color = "blue";
+  document.getElementById("Ck").style.fontFamily = "sans-serif";
+}
+
+function myfunction3() {
+  document.getElementById("Ck").style.color = "green";
+}
